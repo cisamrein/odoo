03d49ff4a7564c488475b78e0b7b4cedae1b3071
samuel <s.ramarosely@etechconsulting-mg.com>
2020-05-11 10:24:04 +0300

    [ADD] CISA-20 default configuration colis and multi route

--- cisamrein_stock/__manifest__.py
+++ cisamrein_stock/__manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Operations/Inventory',
+    'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'data/config_data.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
