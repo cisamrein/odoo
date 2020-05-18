0352314b60c70bdb63493d20251def213d503e1f
Laza <etech.laza@gmail.com>
2020-05-18 18:07:11 +0300

    [IMP]CIS-18 Correction retour

--- cisamrein_stock/models/stock_move.py
+++ cisamrein_stock/models/stock_move.py
@@ -2,7 +2,6 @@
 
 from odoo import models, fields, api
 
-
 class StockMoveInherit(models.Model):
     _inherit = 'stock.move'
 
@@ -10,6 +9,7 @@ class StockMoveInherit(models.Model):
     product_code = fields.Char(related='product_id.name', string="Product code")
     product_default_code = fields.Char(related='product_id.default_code', string="Internal Reference")
     material_origin = fields.Many2one('res.country', compute='_compute_material_origin')
+    ref_supplier = fields.Char()
 
     @api.depends('product_id')
     def _compute_material_origin(self):
@@ -17,3 +17,12 @@ class StockMoveInherit(models.Model):
             product_supplierinfo_id = self.env['product.supplierinfo'].search([('product_tmpl_id', '=',
                                                                                  rec.product_id.product_tmpl_id.id)], limit=1)
             rec.material_origin = product_supplierinfo_id.name.country_id.id
+
+    @api.model
+    def create(self, values):
+        # Add code here
+        res = super(StockMoveInherit, self).create(values)
+        if res.purchase_line_id:
+            res.item = res.purchase_line_id.item
+            res.ref_supplier = res.purchase_line_id.ref_supplier
+        return res
