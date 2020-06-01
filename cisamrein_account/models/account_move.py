9063e3dc297660140688236e8accc48c9b631e90
Laza <etech.laza@gmail.com>
2020-06-01 18:48:01 +0300

    [FIX] Fix bug on test failed

--- cisamrein_account/models/account_move.py
+++ cisamrein_account/models/account_move.py
@@ -60,24 +60,24 @@ class AccountInvoiceLine(models.Model):
                                                                               limit=1, order='id DESC')
             rec.customer_ref = product_customerinfo_id.name
 
-    def _get_computed_name(self):
-        self.ensure_one()
-
-        if not self.product_id:
-            return ''
-
-        if self.partner_id.lang:
-            product = self.product_id.with_context(lang=self.partner_id.lang)
-        else:
-            product = self.product_id
-
-        values = []
-        if product.partner_ref:
-            values.append(product.partner_ref)
-        if self.journal_id.type == 'sale':
-            if product.description_sale:
-                values.append(product.description_sale)
-        elif self.journal_id.type == 'purchase':
-            if product.description_purchase:
-                values.append(product.description_purchase)
-        return values
+    # def _get_computed_name(self):
+    #     self.ensure_one()
+    #
+    #     if not self.product_id:
+    #         return ''
+    #
+    #     if self.partner_id.lang:
+    #         product = self.product_id.with_context(lang=self.partner_id.lang)
+    #     else:
+    #         product = self.product_id
+    #
+    #     values = []
+    #     if product.partner_ref:
+    #         values.append(product.partner_ref)
+    #     if self.journal_id.type == 'sale':
+    #         if product.description_sale:
+    #             values.append(product.description_sale)
+    #     elif self.journal_id.type == 'purchase':
+    #         if product.description_purchase:
+    #             values.append(product.description_purchase)
+    #     return values
