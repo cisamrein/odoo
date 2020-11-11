561f23c45dead69c185444f9910962a739a7033d
LAZA-ANDRIAMANANTENA <65153980+LAZA-ANDRIAMANANTENA@users.noreply.github.com>
2020-11-11 13:40:13 +0300

    [FIX] fix bug on installation

--- cisamrein_app/__manifest__.py
+++ cisamrein_app/__manifest__.py
@@ -20,7 +20,8 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['cisamrein_base', 'cisamrein_product', 'cisamrein_stock', 'cisamrein_sale', 'cisamrein_purchase', 'cisamrein_account', 'repair'],
+    'depends': ['cisamrein_base', 'cisamrein_product', 'cisamrein_stock', 'cisamrein_sale', 'cisamrein_purchase', 'cisamrein_account',
+                'repair', 'mrp'],
     'sequence': 1,
     # always loaded
     'data': [
--- cisamrein_product/__manifest__.py
+++ cisamrein_product/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['product', 'mrp', 'cisamrein_base'],
+    'depends': ['product', 'cisamrein_base'],
 
     # always loaded
     'data': [
