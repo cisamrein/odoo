3f8739ead04b42d102fd193ae1287475cc4e05e6
LAZA-ANDRIAMANANTENA <65153980+LAZA-ANDRIAMANANTENA@users.noreply.github.com>
2020-11-12 14:18:12 +0300

    [FIX] fix depends and delevery sequence

--- cisamrein_stock/models/stock_picking.py
+++ cisamrein_stock/models/stock_picking.py
@@ -12,12 +12,7 @@ class StockPickingInherit(models.Model):
     @api.model
     def create(self, vals):
         res = super(StockPickingInherit, self).create(vals)
-        if self.env['sale.order'].search([('name', '=', self.origin)]):
-            old_name = self.name
-            if self.picking_type_id.code == 'PICK':
-                self.name = "PICK_{}/{}.041".format(old_name, self.origin)
-            elif self.picking_type_id.code == 'PACK':
-                self.name = "PACK_{}/{}.041".format(old_name, self.origin)
-            else:
-                self.name = "{}/{}.041".format(old_name, self.origin)
+        old_name = res.name
+        if res.picking_type_code:
+            res.name = "{} - {}.041".format(old_name, res.origin)
         return res
